6b15b55f4c52d5e2b38f4d5b8e9aa833672b7f80
Dejan Golja <dejan@golja.org>
2013-09-27 20:26:24 +1000

    Added spec tests for the module

---
+++ spec/classes/s3cmd_init_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+
+describe 's3cmd', :type => :class do
+  let(:facts) { {:osfamily => 'Debian'} }
+  it { should create_class('s3cmd') }
+    
+  context 'with default settings' do
+    it { should contain_package('s3cmd').with('ensure' => 'present') }
+  end
+  
+  context 'with setting ensure to absent' do
+    let(:params) { { :ensure  => 'absent',} }       
+    it { should contain_package('s3cmd').with('ensure' => 'absent')}
+  end
+    
+  context 'with invalid ensure => installed' do
+    let(:params) { { :ensure => 'installed'} }
+    it 'should fail' do
+      expect { subject }.to raise_error(/ensure must be either present or absent/)
+    end
+  end
+  
+  ['5', '6'].each do |os_major_release|
+    context 'with osfamily: RedHat major release #{os_major_release}' do
+      let(:facts) { {:osfamily => 'RedHat', :operatingsystemmajrelease => os_major_release} }
+      it { should contain_yumrepo('s3tools').with(
+        'baseurl'  => "http://s3tools.org/repo/RHEL_#{os_major_release}/",
+        'gpgkey'   => "http://s3tools.org/repo/RHEL_#{os_major_release}/repodata/repomd.xml.key",
+        'gpgcheck' => '1',
+        'enabled'  => '1'
+      ) }
+      end
+   end
+   
+   context 'with $::operatingsystem Amazon' do
+     let(:facts) { {:osfamily => 'Linux', :operatingsystem => 'Amazon'} }
+     it { should contain_yumrepo('s3tools').with(
+       'baseurl'  => "http://s3tools.org/repo/RHEL_6/",
+       'gpgkey'   => "http://s3tools.org/repo/RHEL_6/repodata/repomd.xml.key",
+       'gpgcheck' => '1',
+       'enabled'  => '1'
+     ) }
+   end
+   
+  context 'with unsupported $::osfamily = foo' do
+    let(:facts) { {:osfamily => 'foo'} }
+
+    it 'should fail' do
+      expect { subject }.to raise_error(/Osfamily foo is not supported/)
+    end
+  end   
+
+  context 'with unsupported operatingsystem in Linux os family' do
+    let(:facts) { {:osfamily => 'Linux', :operatingsystem => 'foo'} }
+
+    it 'should fail' do
+      expect { subject }.to raise_error(/Osfamily Linux with operating system foo is not supported/)
+    end
+  end 
+    
+end
---
+++ spec/defines/s3cmd_config_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+describe 's3cmd::config', :type => :define do
+  let(:title) { 'foo' }
+    
+  let(:default) { { :aws_access_key => 'ASmasdADS', :aws_secret_key => 'mADDFSDS' } }
+  
+  context 'with default values and non root user' do
+    let(:params) { default }
+    it { should contain_file('/home/foo/.s3cfg') }
+    it { should contain_file('/home/foo/.s3cfg').with_content(/^\s*gpg_passphrase =/) }
+    it { should contain_file('/home/foo/.s3cfg').with_content(/^\s*gpg_command = \/usr\/bin\/gpg/) }
+    it { should contain_file('/home/foo/.s3cfg').with_content(/^\s*bucket_location = US/) }
+    it { should contain_file('/home/foo/.s3cfg').with_content(/^\s*use_https = True/) }
+  end
+  
+  context 'with aws_access_key => ASmasdADS and aws_secret_key => mADDFSDS' do
+    let(:params) { { :aws_access_key => 'ASmasdADS', :aws_secret_key => 'mADDFSDS' } }
+    it { should contain_file('/home/foo/.s3cfg').with_content(/^\s*access_key = ASmasdADS/).with_content(/^\s*secret_key = mADDFSDS/) }
+  end
+  
+  context 'with default values and root user' do
+    let(:params) { default.merge({ :user => 'root' })}
+    it { should contain_file('/root/.s3cfg') }
+    it { should contain_file('/root/.s3cfg').with_content(/^\s*gpg_passphrase =/) }
+    it { should contain_file('/root/.s3cfg').with_content(/^\s*gpg_command = \/usr\/bin\/gpg/) }
+  end
+  
+  context 'with home_dir => /opt/home/foo' do
+    let(:params) { default.merge({ :home_dir => '/opt/home/foo' })}
+    it { should contain_file('/opt/home/foo/.s3cfg') }   
+  end
+  
+  context 'with use_https => true' do
+    let(:params) { default.merge({ :use_https => true })}   
+    it { should contain_file('/home/foo/.s3cfg').with_content(/^\s*use_https = True/) }
+  end
+ 
+  context 'with use_https => false' do
+    let(:params) { default.merge({ :use_https => false })}
+    it { should contain_file('/home/foo/.s3cfg').with_content(/^\s*use_https = False/) }
+  end
+  
+  context 'with bucket_location => EU' do
+    let(:params) { default.merge({ :bucket_location => 'EU' })}
+    it { should contain_file('/home/foo/.s3cfg').with_content(/^\s*bucket_location = EU/) }
+  end
+
+  context 'with encryption_passphrase => blabla' do
+    let(:params) { default.merge({ :encryption_passphrase => 'blabla' })}
+    it { should contain_file('/home/foo/.s3cfg').with_content(/^\s*gpg_passphrase = blabla/) }
+  end
+    
+end
